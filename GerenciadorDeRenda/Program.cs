ddb1f3f4a69ff3bea6796128dfe743765563033e
Marcos Felipe Silva Celestino <marcos.felipe@timesharesolucoes.com.br>
2022-10-10 23:11:58 -0300

    banco configurado

--- GerenciadorDeRenda/Program.cs
+++ GerenciadorDeRenda/Program.cs
@@ -1,8 +1,19 @@
+using GerenciadorDeRendaInfra.Context;
+using Microsoft.EntityFrameworkCore;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+//services.AddDbContext<ApplicationDbContext>(options =>
+//options.UseNpgsql(
+//                  Configuration.GetConnectionString("stringConnection")));
+//services.AddDatabaseDeveloperPageExceptionFilter();
+builder.Services.AddDbContext<AppDbContext>(opts => opts.UseNpgsql(
+         builder.Configuration.GetConnectionString("strConnection")));
+    
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
---
+++ GerenciadorDeRendaInfra/Context/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace GerenciadorDeRendaInfra.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    }
+    public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            optionsBuilder.UseNpgsql("User ID = postgres;Password=12345;Host=localhost;Port=5432;Database=postgres");
+
+            return new AppDbContext(optionsBuilder.Options);
+        }
+
+    
+    }
+
+}
