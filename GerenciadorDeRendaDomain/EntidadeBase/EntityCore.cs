fc5dc1cdb795571f9af948d29b45dcd4584ed02d
Victor Alves Farias <victoralvesfarias2004@gmail.com>
2022-10-20 18:06:14 -0300

    Delete GerenciadorDeRendaDomain directory

--- GerenciadorDeRendaDomain/EntidadeBase/EntityCore.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GerenciadorDeRendaDomain.EntidadeBase
-{
-    public class EntityCore
-    {
-        public string Id { get; set; }
-        public DateTime DataCriacao { get; set; }
-        public DateTime DataAlteracao { get; set; }
-    }
-}
